ea3364e170cff81d0a5c555ec059ab089a44e302
Kenicia Harris <keniciah@gmail.com>
2019-01-26 16:27:53 -0600

    Create basicarray.java
    Array class.

---
+++ basicarray.java
@@ -0,0 +1,32 @@
+public class Arrays {
+	private int[] exampleArray;
+	private int length;
+	
+	public Arrays(int length) {
+		this.setLength(length);
+		exampleArray = new int[length];
+		fillArray();
+	}
+	
+	private void fillArray() {
+		for(int i = 0; i < exampleArray.length; i++) {
+			exampleArray[i] = (int)( Math.random() * 10) + 1;
+		}
+	}
+	
+	public void printArray() {
+		System.out.println("Array:");
+		for(int i = 0; i < exampleArray.length; i++) {
+			System.out.print(exampleArray[i] + " ");
+		}
+		System.out.println("");
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+}
